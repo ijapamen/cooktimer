32d9202f04dd7ca64ff44ff4d678a91d754b6053
ijapamen <ijapamen@icloud.com>
2021-01-02 22:32:23 +0900

    画像を保存するための実装

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,4 +1,10 @@
 class RecipesController < ApplicationController
   def index
   end
+
+  private
+
+  def recipe_params
+    params.require(:recipe).permit(:image).merge(user_id: current_user_id)
+  end
 end
