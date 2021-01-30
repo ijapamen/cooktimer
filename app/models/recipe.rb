21cd302a8d1e825be9f975843bd9c81757c39673
ijapamen <ijapamen@icloud.com>
2021-01-30 16:32:35 +0900

    投稿画像のリサイズとバリデーションの追加

--- app/models/recipe.rb
+++ app/models/recipe.rb
@@ -5,6 +5,20 @@ class Recipe < ApplicationRecord
   extend ActiveHash::Associations::ActiveRecordExtensions
   belongs_to :person
 
+  validate :image_content_type, :image_size, if: Proc.new {|recipe| recipe.image.present?}
+  
+  def image_content_type
+    if !image.content_type.in?(%('image/png', 'image/jpg', 'image/jpeg'))
+      errors.add(:image, "にはjpegまたはpngファイルを添付してください")
+    end
+  end
+
+  def image_size
+    if image.blob.byte_size > 5.megabytes
+      errors.add(:image, "は5MB以内にしてください")
+    end
+  end
+
   with_options presence: true do
     validates :image
     validates :name
@@ -13,6 +27,10 @@ class Recipe < ApplicationRecord
     validates :step1
   end
 
+  
+  
+  
   # 人数分の選択が「--」の時は保存できないようにする
   validates :person_id, numericality: { other_than: 0 }
+
 end
