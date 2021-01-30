68a72509c4bfeb664fcae7d95d1d2209f38ac54e
ijapamen <ijapamen@icloud.com>
2021-01-31 07:12:45 +0900

    ゲストログイン機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -9,4 +9,13 @@ class User < ApplicationRecord
   validates :nickname, presence: true
   PASSWORD_REGEX = /\A(?=.*?[a-z])(?=.*?\d)[a-z\d]+\z/i.freeze
   validates :password, format: { with: PASSWORD_REGEX }
+
+  def self.guest
+    find_or_create_by(email: 'guest@sample.com') do |user|
+      user.password = SecureRandom.urlsafe_base64
+      user.password_confirmation = user.password
+      user.nickname = "guest"
+    end
+  end
+
 end
